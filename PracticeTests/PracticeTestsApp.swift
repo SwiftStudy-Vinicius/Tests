7c45b3ad8b24ca9cb064d2fa7cd1664da191c61d
Vinícius <viniciusfloresribeiro@gmail.com>
2023-02-28 16:38:24 -0300

    create project

---
+++ PracticeTests/PracticeTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticeTestsApp.swift
+//  PracticeTests
+//
+//  Created by Vinícius Flores Ribeiro on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PracticeTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
