fca699f536c474b2e12822e3e717d53feb3d3f12
Vinícius <viniciusfloresribeiro@gmail.com>
2023-03-01 11:40:35 -0300

    Refine tests

--- ConvertersTests/ConvertersTests.swift
+++ ConvertersTests/ConvertersTests.swift
@@ -8,11 +8,23 @@
 import XCTest
 
 final class ConvertersTests: XCTestCase {
+    /// AAA: Arrange - Act - Assert
+
+    private var converter: Converters!
+
+    // Will be occur before the func
+    override func setUpWithError() throws {
+        converter = Converters()
+    }
+
+    // Will be occur after the func
+    override func tearDownWithError() throws {
+        converter = nil
+    }
+
 
     // Create a simple conversion with a positive number
     func test_convert10_return1080() {
-        let converter = Converters()
-
         let actual = converter.convertEuroToUSD(euro: "10")
         let expected = "$10.80"
 
@@ -21,8 +33,6 @@ final class ConvertersTests: XCTestCase {
 
     // Negative numbers return an error message
     func test_convertNeg10_retunrsErrorMessage() {
-        let converter = Converters()
-
         let actual = converter.convertEuroToUSD(euro: "-10")
         let expected = "Please enter a positive number"
         XCTAssertEqual(actual, expected)
